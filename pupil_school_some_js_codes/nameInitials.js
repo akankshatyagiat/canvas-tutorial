82c5972591b9473447f2d76ef5b08ec49b83838a
Akanksha Tyagi <fir.akanksha@gmail.com>
2021-05-28 18:04:34 +0530

    added some js codes which was in the exercise

---
+++ pupil_school_some_js_codes/nameInitials.js
@@ -0,0 +1,22 @@
+/* This function should convert a name to initials. Here are few conditions to be met:
+    1. A single worded name must return the first two letters of the word as initials. Eg. John should have an initial JO
+    2. A two worded name must return the first letter of each of the word in the name. Eg. John Doe should have an initial JD
+    3. A name with more words should return the starting letter of the first and the last word as initial. Eg. John Doe Honai should have an initial JH
+    4. The function should return all initials in uppercase.
+*/
+
+function createInitialsFromName(name) {
+  // Complete the function
+  let printInitials="";
+  let partsOfName=name.split(" ");
+  let lengthOfName=partsOfName.length;
+  if(partsOfName.length===1)
+      printInitials+=partsOfName[0].slice(0,2);
+  else if(partsOfName.length===2)
+      printInitials+=partsOfName[0][0]+partsOfName[1][0];
+  else if(partsOfName.length>2)
+      printInitials+=partsOfName[0][0]+partsOfName[partsOfName.length-1][0];
+  return printInitials.toUpperCase();
+}
+
+module.exports = createInitialsFromName;
